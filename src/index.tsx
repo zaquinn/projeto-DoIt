48eb91a6b72a8bffd31cce5983c3c23c212aa469
Isaac Xavier <ayzick120@gmail.com>
2022-05-30 19:05:00 -0300

    create: initial setup with chakra-ui

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { theme } from "./styles/theme";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
