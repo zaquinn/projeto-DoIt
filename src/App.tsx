9c7b3231d0ec0cd5cfa534fc296a70659074022e
Isaac Xavier <ayzick120@gmail.com>
2022-06-17 14:22:53 -0300

    create: base project with Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Routes } from "./routes";
+
 export const App = () => {
-  return <h1>App</h1>;
+  return <Routes />;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import { theme } from "./styles/theme";
+import { AppProvider } from "./contexts";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
