1c1292212f2d3fe1e27791d112d65c6aa066c3bb
Isaac Xavier <ayzick120@gmail.com>
2022-06-18 21:57:51 -0300

    create: created and finished login page

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,96 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+  InputLeftElement,
+  InputGroup,
+} from "@chakra-ui/react";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  ForwardRefRenderFunction,
+  forwardRef,
+} from "react";
+import { FieldError } from "react-hook-form";
+import { IconType } from "react-icons";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  error?: FieldError | null;
+  icon?: IconType;
+}
+
+type inputVariationOptions = {
+  [key: string]: string;
+};
+
+const inputVariation: inputVariationOptions = {
+  error: "red.500",
+  default: "gray.200",
+  focus: "purple.800",
+  filled: "green.500",
+};
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, error = null, icon: Icon, label, ...rest },
+  ref
+) => {
+  const [variation, setVariation] = useState("default");
+
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (error) {
+      return setVariation("error");
+    }
+  }, [error]);
+
+  const handleInputFocus = useCallback(() => {
+    if (!error) {
+      setVariation("focus");
+    }
+  }, [error]);
+
+  const handleInputBlur = useCallback(() => {
+    if (value.length > 1 && !error) {
+      return setVariation("filled");
+    }
+  }, [error, value]);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel>{label}</FormLabel>}
+      <InputGroup flexDirection="column">
+        {Icon && (
+          <InputLeftElement color={inputVariation[variation]} mt="2.5">
+            <Icon />
+          </InputLeftElement>
+        )}
+        <ChakraInput
+          name={name}
+          bg="gray.50"
+          variant="outline"
+          _hover={{ bgColor: "gray.100" }}
+          _placeholder={{ color: "gray.300" }}
+          size="lg"
+          h="60px"
+          color={inputVariation[variation]}
+          borderColor={inputVariation[variation]}
+          onFocus={handleInputFocus}
+          onBlurCapture={handleInputBlur}
+          onChangeCapture={(e) => setValue(e.currentTarget.value)}
+          ref={ref}
+          {...rest}
+        />
+
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      </InputGroup>
+    </FormControl>
+  );
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,149 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import LogoPrimary from "../../assets/logo-primary.svg";
+import { Input } from "../../components/Form/Input";
+import { FaEnvelope, FaLock } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
 
-export const Login = () => <Heading>Login</Heading>;
+const signInSchema = yup.object().shape({
+  email: yup.string().required("Email obrigatório").email("Email inválido"),
+  password: yup.string().required("Senha obrigatória"),
+});
+
+interface SignInData {
+  email: string;
+  password: string;
+}
+
+export const Login = () => {
+  const [loading, setLoading] = useState(false);
+
+  const { signIn } = useAuth();
+
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<SignInData>({
+    resolver: yupResolver(signInSchema),
+  });
+
+  const handleSignIn = (data: SignInData) => {
+    setLoading(true);
+    signIn(data)
+      .then((_) => setLoading(false))
+      .catch((err) => setLoading(false));
+  };
+
+  return (
+    <Flex
+      padding={["10px 15px", "10px 15px", "0px"]}
+      alignItems="center"
+      height={["auto", "auto", "100vh", "100vh"]}
+      justifyContent="center"
+      bgGradient={[
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-r, purple.800 65%, white 35%)",
+      ]}
+      color="white"
+    >
+      <Flex
+        w={["100%", "100%", "90%", "65%"]}
+        justifyContent="center"
+        flexDirection={["column", "column", "row", "row"]}
+        alignItems="center"
+      >
+        <Grid w={["100%", "100%", "50%", "50%"]} paddingRight="100px">
+          <Image
+            src={LogoPrimary}
+            alt="Logo do Doit"
+            boxSize={["120px", "120px", "150px"]}
+          />
+          <Heading as="h1" mt="4">
+            O jeito fácil, grátis
+          </Heading>
+          <Text maxWidth="350px">
+            Fléxivel e atrativo de gerenciar{" "}
+            <b>seus projetos em uma única plataforma</b>
+          </Text>
+        </Grid>
+        <Grid
+          as="form"
+          mt={["4", "4", "0"]}
+          w={["100%", "80%", "60%", "60%", "40%"]}
+          padding="30px 15px"
+          border="3px solid"
+          borderColor="gray.100"
+          bg="white"
+          color="gray.900"
+          onSubmit={handleSubmit(handleSignIn)}
+        >
+          <Heading size="lg">Bem vindo de volta!</Heading>
+          <VStack spacing="5" mt="5">
+            <Box w="100%">
+              <Input
+                icon={FaEnvelope}
+                placeholder="Digite seu login"
+                label="Login"
+                type="email"
+                error={errors.email}
+                {...register("email")}
+              />
+              {!errors.email && (
+                <Text ml="1" mt="1" color="gray.300">
+                  Exemplo: nome@email.com
+                </Text>
+              )}
+            </Box>
+            <Input
+              icon={FaLock}
+              label="Senha"
+              placeholder="Digite sua senha"
+              error={errors.password}
+              type="password"
+              {...register("password")}
+            />
+          </VStack>
+          <VStack mt="4" spacing="5">
+            <Button
+              isLoading={loading}
+              bg="purple.800"
+              w="100%"
+              color="white"
+              h="60px"
+              borderRadius="8px"
+              _hover={{ bg: "purple.900" }}
+              type="submit"
+            >
+              Entrar
+            </Button>
+            <Text color="gray.400">Ainda não possui uma conta?</Text>
+            <Button
+              bg="gray.100"
+              w="100%"
+              color="gray.300"
+              h="60px"
+              borderRadius="8px"
+              _hover={{ bg: "gray.200" }}
+            >
+              Cadastrar
+            </Button>
+          </VStack>
+        </Grid>
+      </Flex>
+    </Flex>
+  );
+};
