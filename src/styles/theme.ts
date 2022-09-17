49e497a988056d1aaa00dceadf8a42ff29757f51
realiz3d <ayzick120@gmail.com>
2022-09-17 01:08:51 -0300

    hotfix: fixed theme.ts, putting properties out of colors property

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -22,31 +22,31 @@ export const theme = extendTheme({
     green: {
       600: "#168821",
     },
-    fonts: {
-      heading: "Inter",
-      body: "Inter",
-    },
-    fontSizes: {
-      xs: "0.75rem",
-      sm: "0.875rem",
-      md: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
-      "6xl": "3.75rem",
-      "7xl": "4.5rem",
-      "8xl": "6rem",
-      "9xl": "8rem",
-    },
-    styles: {
-      global: {
-        body: {
-          bg: "white",
-          color: "gray.900",
-        },
+  },
+  fonts: {
+    heading: "Inter",
+    body: "Inter",
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6rem",
+    "9xl": "8rem",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "white",
+        color: "gray.900",
       },
     },
   },
